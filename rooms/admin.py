0b1a735bc3bf0fbe1165dfcb72ae4dafd2edf426
Mariola <marwrobel3@gmail.com>
2026-02-14 21:06:48 +0100

    Add rooms model with capacity and active flag

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from rooms.models import Room
 # Register your models here.
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('name', 'capacity', 'location', 'is_active')
+    list_filter = ('is_active',)
+    search_fields = ('name', 'location')
--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.utils import timezone
+from django.conf import settings
+from django.core.exceptions import ValidationError
 # Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    capacity = models.PositiveIntegerField(default=1)
+    location = models.CharField(max_length=200, blank=True)
+    is_active = models.BooleanField(default=True)
+    def __str__(self):
+        return self.name
