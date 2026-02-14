842411ac2061361b7130c843f17e3a014c269742
Mariola <marwrobel3@gmail.com>
2026-02-14 20:22:50 +0100

    configure admin login, Add rooms and accounts apps

--- accounts/views.py
+++ accounts/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('dzial')
+    return HttpResponse('dziala')
+    #return render(request, 'accounts/home.html')
